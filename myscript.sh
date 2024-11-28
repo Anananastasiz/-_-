df5f25c538e519daed34d3b8f22acab8de50928a
Anananastasiz <ananastasi.vl@gmail.com>
2024-11-28 14:29:51 +0300

    Add files via upload

---
+++ myscript.sh
@@ -0,0 +1,63 @@
+mkdir home\
+mkdir home_ADAM
+cd home_ADAM
+touch floors
+cd floors
+mkdir 1_floor
+mkdir 2_floor
+ls -l
+cd 1_floor
+mkdir holl
+mkdir living_room
+mkdir kitchen
+cd 2_floor
+ls -l
+cd ~/home_ADAM/2_floor/
+cd ~/home_ADAM/1_floor/
+cd ../
+cd 2_floor/
+cd ../
+cd 1_floor 
+cd living_room
+touch closet
+cd closet
+ls
+tree
+rm closet
+ls
+sudo apt install tree
+tree
+mkdir closet
+cd closet
+touch book.txt
+cd kitchen
+cd ../
+cd kitchen
+mkdir fridge
+cd fridge
+touch list_products
+ls -l
+vim list_products
+nano list_products
+cd ../
+cd../
+cd 2_floor
+cd ~/home_ADAM/1_floor/
+cd ~/home_ADAM/2_floor
+mkdir daughter
+mkdir mom
+mkdir papa
+mkdir son
+mkdir grandmother
+ls
+sudo apt install tree
+chmod 777 grandmother
+chmod 777 mom
+chmod 777 son
+chmod 777 papa
+chmod 700 daughter
+tree
+
+
+
+
