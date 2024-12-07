d6c46787ce0c3ef9c02bf3d2ebb52114b69a0304
Anananastasiz <ananastasi.vl@gmail.com>
2024-12-07 23:20:23 +0300

    Update_hw.sh

--- hw.sh
+++ hw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir results
+cd results
 gunzip gencode.v41.basic.annotation.gff3
 awk '$3 == "gene"'  gencode.v41.basic.annotation.gff3 > filtered_genes.tsv
 awk -F'\t' '$9 ~ /gene_type=unprocessed_pseudogene/ {print $1"\t"$4"\t"$5"\t"$7"\t"$9}' "$1" gencode.v41.basic.annotation.gff3 > filtered_genes.tsv
@@ -8,5 +10,5 @@ awk 'BEGIN { FS = OFS = "\t" } { if ($4 == "+") { $3 = $3 + 1 } print }' filtere
 
 awk 'BEGIN { FS = OFS = "\t" } { if ($4 == "-") { temp = $2; $2 = $3; $3 = temp; $4 = "+"; $3 = $3 + 1 } print }' filtered_genes.tsv > result.tsv
 
-mv result.tsv results/
+mv result.tsv root/results
 
